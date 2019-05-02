6820991b38c3c160779fb5d0b241877a04f4889b
Kied Llaentenn <32681240+Kiedtl@users.noreply.github.com>
2019-05-02 19:22:46 -0400

    Add pxl tools

---
+++ src/lib/compress.ps1
@@ -0,0 +1,48 @@
+param (
+    $E = [char]0x1b,
+    [string]$pxl,
+    $out = "$pxl.comp"
+)
+
+$cpxl = @()
+$pixels = @()
+$ctr = 0
+
+# example of $pxl
+# $E[48;2;245;133;111m$E[38;2;123;234;56m▀$E[48;2;174;221;223m$E[38;2;123;234;99m▀$E[0m$E[B$E[0G
+
+foreach($line in Get-Content $pxl) {
+    # get each individual pixel
+    $pixels = $line.TrimEnd("$E[0m$E[B$E[0G").Split('▀')
+    foreach ($pixel in $pixels) {
+        $newpxl = ""
+        if ("" -eq $pixel) {
+            continue
+        }
+        $pixel = $pixel.TrimStart($E)
+
+        $bg_sgr = ($pixel.Split($E)[0])
+        $bg_sgr = $bg_sgr.TrimEnd('m')
+
+        $fg_sgr = ($pixel.Split($E)[1]).TrimEnd('m')
+
+        $mbgr, $mbgg, $mbgb = $bg_sgr.Split(';')[2..4]
+        $mfgr, $mfgg, $mfgb = $fg_sgr.Split(';')[2..4]
+
+        [char]$bgr = [char](([int]$mbgr) + 125)
+        [char]$bgg = [char](([int]$mbgg) + 125)
+        [char]$bgb = [char](([int]$mbgb) + 125)
+        [char]$fgr = [char](([int]$mfgr) + 125)
+        [char]$fgg = [char](([int]$mfgg) + 125)
+        [char]$fgb = [char](([int]$mfgb) + 125)
+
+        $newpxl += "${bgr}${bgg}${bgb}|${fgr}${fgg}${fgb};"
+        $cpxl += $newpxl 
+        $ctr++
+        write-output "pxl-decomp: compressed pixel #$ctr ($($bg_sgr);${fg_sgr}) as $newpxl"
+    }
+    $cpxl += "`n"
+}
+
+"%PXLCv0.0.1" > $out 
+$cpxl -join '' >> $out
---
+++ src/lib/decompress.ps1
@@ -0,0 +1,44 @@
+param (
+    $E = [char]0x1b,
+    [string]$pxl,
+    $out = ("${pxl}.unc")
+)
+
+$cpxl = @()
+$pixels = @()
+$char = '▀'
+
+foreach($line in Get-Content $pxl) {
+    if ($line -match '^%PXLCv((([0-9])+\.?))+$') {
+        continue
+    } 
+    $pixels = $line.Split(';')
+    foreach ($pixel in $pixels) {
+        if ($pixel -eq $null -or ($pixel -eq '')) {
+            continue
+        }
+        $bgsgr = $pixel.split('|')[0]
+        $fgsgr = $pixel.split('|')[1]
+
+        $mbgr = [char]$bgsgr[0]
+        $mbgg = [char]$bgsgr[1]
+        $mbgb = [char]$bgsgr[2]
+        $mfgr = [char]$fgsgr[0]
+        $mfgg = [char]$fgsgr[1]
+        $mfgb = [char]$fgsgr[2]
+
+        [int]$bgr = [int][char](([int]$mbgr) - 125)
+        [int]$bgg = [int][char](([int]$mbgg) - 125)
+        [int]$bgb = [int][char](([int]$mbgb) - 125)
+        [int]$fgr = [int][char](([int]$mfgr) - 125)
+        [int]$fgg = [int][char](([int]$mfgg) - 125)
+        [int]$fgb = [int][char](([int]$mfgb) - 125)
+
+        $cpxl += "$E[48;2;${bgr};${bgg};${bgb}m$E[38;2;${fgr};${fgg};${fgb}m${char}"
+        write-output "pxl-decomp: uncompressed pixel $pixel (bg: $bgsgr fg: $fgsgr) as ${bgr},${bgg},${bgb}"
+    }
+        
+    $cpxl += "$E[0m$E[B$E[0G`n"
+}
+
+(-join $cpxl) > $out
